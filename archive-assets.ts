01d5d112433e9d6e6025f1ee0702309b460779d5
rouanw <rouanw@users.noreply.github.com>
2022-07-01 14:10:24 +0200

    archive assets script

--- archive-assets.ts
+++ archive-assets.ts
@@ -2,13 +2,13 @@ import * as contentful from 'contentful-management';
 import 'dotenv/config';
 import {readFile} from "fs/promises";
 
-async function getEntriesToArchive(file: string) {
+async function getAssetsToArchive(file: string) {
   const contents = await readFile(file, 'utf8');
   const parsed = JSON.parse(contents);
   return Array.isArray(parsed) ? parsed : [];
 }
 
-async function archiveEntries(entryIds: string[]) {
+async function archiveAssets(assetIds: string[]) {
   if (!process.env.CONTENTFUL_SPACE_ID || !process.env.CONTENTFUL_MANAGEMENT_TOKEN) {
     throw ('Please set env vars');
   }
@@ -20,20 +20,20 @@ async function archiveEntries(entryIds: string[]) {
   const space = await client.getSpace(process.env.CONTENTFUL_SPACE_ID);
   const environment = await space.getEnvironment('make_space');
 
-  for (const entryId of entryIds) {
+  for (const assetId of assetIds) {
     try {
-      const entry = await environment.getEntry(entryId);
-      if (entry.isPublished()) {
-        await entry.unpublish();
+      const asset = await environment.getAsset(assetId);
+      if (asset.isPublished()) {
+        await asset.unpublish();
       }
-      await entry.archive();
-      console.log(`Archived ${entryId}`);
+      await asset.archive();
+      console.log(`Archived ${assetId}`);
     } catch (error) {
-      console.error(`Failed to archive ${entryId}`, error);
+      console.error(`Failed to archive ${assetId}`, error);
     }
   }
 }
 
-getEntriesToArchive('./out/test.json')
-  .then(archiveEntries)
+getAssetsToArchive('./archive/example.json')
+  .then(archiveAssets)
   .catch(console.error);
